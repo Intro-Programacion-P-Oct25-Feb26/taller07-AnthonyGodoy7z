7f8c3158136e30664568e722628b38f27650b6dd
Anthony <tonyxiz10@gmail.com>
2025-11-20 19:56:21 -0500

    1

---
+++ problematicas/Ejercicio03/src/ejercicio03/Ejercicio03.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio03;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author UTPL
+ */
+public class Ejercicio03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner entrada = new Scanner(System.in); // Variables
+        entrada.useLocale(Locale.US);
+        String mensaje = "";
+        int contador = 0;
+        int suma = 0;
+        int valor;
+        
+        while (contador <= 5) {
+            contador = contador + 1;
+            valor = (int) (Math.pow(contador, 2) + 1);
+            
+            mensaje = String.format("%s%d\n", mensaje,valor);
+        }
+        System.out.printf("%s\n", mensaje);
+    }
+    
+}
